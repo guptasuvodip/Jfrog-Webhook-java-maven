8c33528f60e32cf61da48fcac8193420f2edba40
Debabrata <devpay2022@gmail.com>
2023-10-08 05:15:52 +0530

    Code push

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Jfrog-Webhook-java-maven
+# Jrog-Webhook-java-maven
 Its a simple test project to create artifeact and send to Jrog
---
+++ src/main/java/com/jfrog-sample-project/jfrog.java
@@ -0,0 +1,7 @@
+package com.Jfrog;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome, Jfrog!");
+    }
+}
