36e04052dc944119716d6741a722da1844795bd4
Debabrata <devpay2022@gmail.com>
2023-10-08 14:23:53 +0530

    Changes in Code

--- src/main/java/com/jfrog-sample-project/com/example/app.java
+++ src/main/java/com/jfrog-sample-project/com/example/app.java
@@ -1,7 +1,7 @@
-package com.example;
+package com.welcome;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Welcome!");
     }
 }
